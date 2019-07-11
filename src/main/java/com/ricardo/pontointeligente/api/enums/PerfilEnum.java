67205a0c8709fd954bbffb29035362f2b95782d9
Ricardo Rocha <ricardo.rocha@mv.com.br>
2019-07-11 11:57:41 -0300

    Mapeamento das entidades de negócio

---
+++ src/main/java/com/ricardo/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,19 @@
+package com.ricardo.pontointeligente.api.enums;
+
+/**
+ * Perfil do usuário.
+ * @author ricardo.rocha
+ */
+public enum PerfilEnum {
+	
+	/**
+	 * Administrador
+	 */
+	ROLE_ADMIN,
+	
+	/**
+	 * Usuário
+	 */
+	ROLE_USUARIO
+	
+}
---
+++ src/main/java/com/ricardo/pontointeligente/api/enums/TipoLancamentoEnum.java
@@ -0,0 +1,17 @@
+package com.ricardo.pontointeligente.api.enums;
+
+/**
+ * Tipo de lançamento no registro de ponto do usuário 
+ * 
+ * @author ricardo.rocha
+ */
+public enum TipoLancamentoEnum {
+	
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	IINCIO_PAUSA,
+	TERMINO_PAUSA;
+
+}
