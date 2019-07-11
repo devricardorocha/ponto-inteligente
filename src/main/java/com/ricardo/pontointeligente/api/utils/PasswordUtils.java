80c7c1c4897fd28ca667834ec9ed539801a3d138
Ricardo Rocha <ricardo.rocha@mv.com.br>
2019-07-11 12:38:07 -0300

    Criptografia de senha com BCrypt

---
+++ src/main/java/com/ricardo/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,27 @@
+package com.ricardo.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public abstract class PasswordUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+
+	/**
+	 * Criptogra senha do usuário.
+	 * 
+	 * @param senha Senha a ser criptografada BASE64
+	 * @return String Senha criptografada
+	 */
+	public static String gerarBCrypt(String senha) {
+		if (senha != null) {
+			log.info("Gerando hash com o BCrypt.");
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			return encoder.encode(senha);
+		}
+
+		return null;
+	}
+
+}
